81316de285cef3a3ce3c3334fbbcc312f6b323c8
Romullo <romullocordeiro@gmail.com>
2021-06-15 23:26:47 -0300

    feat: configure svg in jsx usage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import CasinoLogo from '../../../assets/casion-logo.svg';
+
 const index = () => {
   return (
     <View>
       <Text>Home Screen</Text>
+      <CasinoLogo />
     </View>
   );
 };

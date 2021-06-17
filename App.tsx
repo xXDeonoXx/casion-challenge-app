d9990a66e64345bed10572c99b0e7716a8e5a987
Romullo <romullocordeiro@gmail.com>
2021-06-17 00:09:53 -0300

    fix: add ActivityIndicator return on loading don't finished to prevent usage of fonts prior loading

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Routes from './src/routes';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components';
 import getTheme, { ThemesTypes } from './theme';
 import * as Font from 'expo-font';
+import { ActivityIndicator } from 'react-native';
 
 /**
  * Esse App foi desenvolvido para o seguinte desafio: https://github.com/davilson/casion-tech-goodies-tests/tree/master/casion-test-full-stack
  */
 
 export default function App() {
+  const [loading, setloading] = useState(true);
   useEffect(() => {
     const loadFonts = async () => {
       await Font.loadAsync({
@@ -32,10 +34,16 @@ export default function App() {
         'PT-Serif-BoldItalic': {
           uri: require('./assets/fonts/PT_Serif/PTSerif-BoldItalic.ttf'),
         },
+        'PT-Serif-Italic': {
+          uri: require('./assets/fonts/PT_Serif/PTSerif-Italic.ttf'),
+        },
       });
+      setloading(false);
     };
     loadFonts();
   }, []);
+
+  if (loading) return <ActivityIndicator />;
 
   return (
     <NavigationContainer>
